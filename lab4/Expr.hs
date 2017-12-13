db3208507767d762264cce9c69c21e71ca4a5abf
Vaios Taxiarchis <vtaxiarchis@hotmail.com>
2017-12-13 01:09:55 +0100

    Add implementation for Part C and D

--- lab4/Expr.hs
+++ lab4/Expr.hs
@@ -1,31 +1,130 @@
 module Expr where
+
+import Parsing
+
 import Data.Char
 import Data.Maybe
 
+import Test.QuickCheck
+
+-- Part A
 -- Data type that represents all expressions.
 -- It can be number, variable, addition, multiplication, sin or cos.
-data Expr = Num Double | Var | Add Expr Expr | Mul Expr Expr | Sin Expr | Cos Expr
+data Expr = Num Double | Var |
+            Add Expr Expr | Mul Expr Expr |
+            Sin Expr | Cos Expr
   deriving(Eq)
 
+-- Part B
 -- Function to convert any expression to string.
 showExpr :: Expr -> String
-showExpr (Num num) = show num
+showExpr (Num n) = show n
 showExpr Var = "x"
-showExpr (Add num1 num2) = showExpr num1 ++ "+" ++ showExpr num2
-showExpr (Mul num1 num2) = showExpr'' num1 ++ "*" ++ showExpr'' num2
+showExpr (Add n1 n2) = showExpr n1 ++ "+" ++ showExpr n2
+showExpr (Mul n1 n2) = showExpr'' n1 ++ "*" ++ showExpr'' n2
 showExpr (Sin angle) = "sin " ++ showExpr' angle
 showExpr (Cos angle) = "cos " ++ showExpr' angle
 
+-- Helper function that adds parenthesis as part of functions
 showExpr' :: Expr -> String
-showExpr' (Num num) = show num
+showExpr' (Num n) = show n
 showExpr' Var = "x"
-showExpr' x@(Add num1 num2) = "(" ++ showExpr x ++ ")"
-showExpr' x@(Mul num1 num2) = "(" ++ showExpr'' x ++ ")"
+showExpr' x@(Add _ _) = "(" ++ showExpr x ++ ")"
+showExpr' x@(Mul _ _) = "(" ++ showExpr'' x ++ ")"
 showExpr' x = showExpr x
 
+-- Helper functions that adds parenthesis only for additions as part of multiply
 showExpr'' :: Expr -> String
-showExpr'' (Add num1 num2) = "(" ++ showExpr (Add num1 num2) ++")"
-showExpr'' num = showExpr num
+showExpr'' (Add n1 n2) = "(" ++ showExpr (Add n1 n2) ++")"
+showExpr'' n = showExpr n
 
 instance Show Expr where
   show = showExpr
+
+-- Part C
+-- Function to evaluate an expression given a value for x
+eval :: Expr -> Double -> Double
+eval (Num n) _ = n
+eval Var x = x
+eval (Add n1 n2) x = (eval n1 x) + (eval n2 x)
+eval (Mul n1 n2) x = (eval n1 x) * (eval n2 x)
+eval (Sin n) x = sin (eval n x)
+eval (Cos n) x = cos (eval n x)
+
+-- Part D
+-- Reads a string and converts it to an expression
+readExpr :: String -> Maybe Expr
+readExpr s = do
+             r <- parse expr s
+             return $ fst r
+
+{- EBNF:
+expr   ::= term {"+" term}.
+term   ::= factor {"*" factor}.
+factor ::= number | variable | "(" expr ") | sin factor | cos factor"
+
+We are following the same structure presented in lectures.
+-}
+expr, term, factor :: Parser Expr
+
+expr = do t <- term
+          ts <- zeroOrMore rest
+          return (foldl Add t ts)
+          where rest = do skipSpaces
+                          char '+'
+                          skipSpaces
+                          next <- term
+                          skipSpaces
+                          return next
+
+term = do t <- factor
+          ts <- zeroOrMore rest
+          return (foldl Mul t ts)
+          where rest = do skipSpaces
+                          char '*'
+                          skipSpaces
+                          next <- factor
+                          skipSpaces
+                          return next
+
+factor = numberParser <|>
+         variableParser <|>
+         parenthesisParser <|>
+         functionParser Sin "sin" <|>
+         functionParser Cos "cos"
+
+-- Parser for parenthesis expressions
+parenthesisParser :: Parser Expr
+parenthesisParser = do char '('
+                       e <- expr
+                       char ')'
+                       return e
+
+-- Parser to skip spaces in an expression
+skipSpaces :: Parser String
+skipSpaces = zeroOrMore (sat isSpace)
+
+-- Parser for functions, captures function parameter
+functionParser :: (Expr -> b) -> String -> Parser b
+functionParser f s = do discardName
+                        skipSpaces
+                        e <- factor
+                        return (f e)
+                     where discardName = sequence_ (replicate (length s) item)
+
+-- Parser for variables
+variableParser :: Parser Expr
+variableParser = do char 'x'
+                    return Var
+
+-- Parser for numbers
+numberParser :: Parser Expr
+numberParser = do n <- readsP :: Parser Double
+                  return (Num n)
+
+-- Property that checks whether generated string can be parsed back
+prop_Parsable :: Expr -> Bool
+prop_Parsable e = case parsed of Just p -> p == e
+                                 Nothing -> False
+                  where parsed = readExpr (showExpr e)
+
